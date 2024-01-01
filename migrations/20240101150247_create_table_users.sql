ab4b1ebbc2ee556abc4aa4f88dd163707e48e4ef
Phathdt <phathdt379@gmail.com>
2024-01-02 00:32:55 +0700

    add sample data

--- migrations/20240101150247_create_table_users.sql
+++ migrations/20240101150247_create_table_users.sql
@@ -6,6 +6,12 @@ CREATE TABLE "users" (
     "created_at" timestamp(0) DEFAULT now(),
     "updated_at" timestamp(0) DEFAULT now()
 );
+
+INSERT INTO "public"."users" ("id", "name", "created_at", "updated_at") VALUES
+(1, 'Alice', '2024-01-01 15:07:38', '2024-01-01 15:07:38'),
+(2, 'Bill', '2024-01-01 15:07:38', '2024-01-01 15:07:38'),
+(3, 'Charles', '2024-01-01 15:07:38', '2024-01-01 15:07:38');
+
 -- +goose StatementEnd
 
 -- +goose Down
--- migrations/20240101150255_create_table_accounts.sql
+++ migrations/20240101150255_create_table_accounts.sql
@@ -10,6 +10,18 @@ CREATE TABLE "accounts" (
 );
 
 ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+INSERT INTO "public"."accounts" ("id", "user_id", "name", "balance", "created_at", "updated_at") VALUES
+(1, 1, 'Alice first account', 10000, '2024-01-01 15:08:23', '2024-01-01 15:08:23'),
+(2, 1, 'Alice second account', 10000, '2024-01-01 15:08:23', '2024-01-01 15:08:23'),
+(3, 1, 'Alice third account', 10000, '2024-01-01 15:08:23', '2024-01-01 15:08:23'),
+(4, 2, 'Bill first account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02'),
+(5, 2, 'Bill second account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02'),
+(6, 2, 'Bill third account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02'),
+(7, 3, 'Charles first account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02'),
+(8, 3, 'Charles second account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02'),
+(9, 3, 'Charles third account', 10000, '2024-01-01 15:09:02', '2024-01-01 15:09:02');
+
 -- +goose StatementEnd
 
 -- +goose Down
